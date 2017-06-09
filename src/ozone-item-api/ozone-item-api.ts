843b5f80de5d578acf6db28f72eb64cfb9cdb3d8
Hubert Jacquin <hj@taktik.com>
2017-06-09 11:02:19 +0200

    Use of generator for AOI collection of items

--- src/ozone-item-api/ozone-item-api.ts
+++ src/ozone-item-api/ozone-item-api.ts
@@ -10,6 +10,12 @@ export type Item = any;
 export interface DomElements {
     ozoneAccess:IronAjax
 }
+export interface BulkResponse {
+    response:Array<Item>;
+}
+export interface ItemResponse {
+    response:Item;
+}
 
 @customElement('ozone-item-api')
 export class OzoneItemAPI  extends OzoneApiAjaxMixin(Polymer.Element){
@@ -22,7 +28,7 @@ export class OzoneItemAPI  extends OzoneApiAjaxMixin(Polymer.Element){
 
     static get observers() {
         return [
-        '_collectionChange(collection, config.endPoints.*)'];
+            '_collectionChange(collection, config.endPoints.*)'];
     }
 
     _collectionChange(collection: string, endpoints: any): void{
@@ -38,7 +44,7 @@ export class OzoneItemAPI  extends OzoneApiAjaxMixin(Polymer.Element){
 
     update(data:any): Promise<Item> {
         const url = this._buildUrl('');
-        return this._postRequest(url, data);
+        return this._postRequest(url, data, this._readItemResponse);
     }
 
     getOne(id:uuid):Promise<Item> {
@@ -51,47 +57,59 @@ export class OzoneItemAPI  extends OzoneApiAjaxMixin(Polymer.Element){
         return this._deleteRequest(url);
     }
 
-    bulkGet(ids:Array<uuid>):Promise<Array<Item>> {
+    bulkGet(ids:Array<uuid>):Promise<Iterator<Item>> {
         const url = this._buildUrl('bulkGet');
-        return this._postRequest(url, ids);
+        return this._postRequest(url, ids, this._readBulkItemResponse);
     }
 
     bulkDelete(ids:Array<uuid>):Promise<Array<uuid>> {
         const url = this._buildUrl('bulkDelete');
-        return this._postRequest(url, ids);
+        return this._postRequest(url, ids, this._readItemResponse);
     }
 
-    bulkSave(items:Array<any>):Promise<Array<Item>> {
+    bulkSave(items:Array<any>):Promise<Iterator<Item>> {
         const url = this._buildUrl('bulkSave');
-        return this._postRequest(url, items);
+        return this._postRequest(url, items, this._readBulkItemResponse);
     }
 
-    _postRequest(url:string, body:any): Promise<any> {
-    this.$.ozoneAccess.url = url;
-    this.$.ozoneAccess.method = 'POST';
-    this.$.ozoneAccess.body = JSON.stringify(body);
-    return this.$.ozoneAccess
-        .generateRequest().completes.then((res:any) => res.response)
+    _readItemResponse = (res:ItemResponse):Item => {
+        return res.response;
+    };
+
+    _readBulkItemResponse =  (res:BulkResponse) => {
+        return (function* ():Iterator<Item> {
+            for (let item of res.response) {
+                yield item;
+            }
+        })();
+    };
+
+    _postRequest(url:string, body:any, responseFilter:any): Promise<any> {
+        this.$.ozoneAccess.url = url;
+        this.$.ozoneAccess.method = 'POST';
+        this.$.ozoneAccess.body = JSON.stringify(body);
+        return this.$.ozoneAccess
+            .generateRequest().completes.then(responseFilter)
     }
 
     _getRequest(url:string): Promise<any> {
-        console.log('_getRequest', url)
-    this.$.ozoneAccess.url = url;
-    this.$.ozoneAccess.method = 'GET';
-    return this.$.ozoneAccess
-        .generateRequest().completes.then((res:any) => res.response)
+        this.$.ozoneAccess.url = url;
+        this.$.ozoneAccess.method = 'GET';
+        return this.$.ozoneAccess
+            .generateRequest().completes.then((res:any) => res.response)
     }
 
     _deleteRequest(url:string): Promise<any> {
-    this.$.ozoneAccess.url = url;
-    this.$.ozoneAccess.method = 'DELETE';
-    return this.$.ozoneAccess
-        .generateRequest().completes.then((res:any) => res.response)
+        this.$.ozoneAccess.url = url;
+        this.$.ozoneAccess.method = 'DELETE';
+        return this.$.ozoneAccess
+            .generateRequest().completes.then((res:any) => res.response)
     }
 
     _buildUrl(action:string):string{
         return `${this.serviceUrl}/${action}`;
-
     }
+
+
 
 }
