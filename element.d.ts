e96a51721d8f2ae6bb92e69d9567558957d5be06
Hubert Jacquin <hj@taktik.com>
2017-07-14 11:21:11 +0200

    create modules from template

---
+++ element.d.ts
@@ -0,0 +1,17 @@
+/**
+ * Custom Elements v1
+ *
+ * Based on https://www.w3.org/TR/2016/WD-custom-elements-20160830/
+ */
+interface CustomElementRegistry {
+    define(name: string, constructor: Function, options?: ElementDefinitionOptions): void;
+    get(name: string): any;
+    whenDefined(name: string): Promise<void>;
+}
+interface ElementDefinitionOptions {
+    extends: string;
+}
+interface CustomEventInit extends EventInit {
+    detail?: any;
+    composed?: boolean;
+}
