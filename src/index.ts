25df515509bd5ac43b5c9788ff7ced59012aae0d
Hubert Jacquin <hj@taktik.com>
2017-10-09 15:38:47 +0200

    add decorator for properties copy form https://github.com/Polymer/polymer-decorators/blob/master/src/decorators.ts

--- src/index.ts
+++ src/index.ts
@@ -31,10 +31,20 @@ export function jsElement() {
                                 // Useful for import in pure JS project.
   }
 }
-
+/**
+ * Options for the @property decorator.
+ * See https://www.polymer-project.org/2.0/docs/devguide/properties.
+ */
 export interface PropertyOptions {
-  notify?: boolean;
-};
+    /**
+     * This field can be omitted if the Metadata Reflection API is configured.
+     */
+    type?: BooleanConstructor|DateConstructor|NumberConstructor|StringConstructor|
+        ArrayConstructor|ObjectConstructor;
+    notify?: boolean;
+    reflectToAttribute?: boolean;
+    readOnly?: boolean;
+}
 
 /**
  * A TypeScript property decorator factory that defines this as a Polymer
@@ -42,22 +52,36 @@ export interface PropertyOptions {
  *
  * This function must be invoked to return a decorator.
  */
-export function property<T>(options?: PropertyOptions) {
-  return (proto: any, propName: string) : any => {
-    const notify = (options && options.notify) as boolean
-    const type = Reflect.getMetadata("design:type", proto, propName);
-    const config = _ensureConfig(proto);
-    config.properties[propName] = {
-      type,
-      notify,
-    };
-  }
-}
+export function property(options?: PropertyOptions) {
+    return (proto: any, propName: string): any => {
+        const notify: boolean = options && options.notify || false;
+        const reflectToAttribute: boolean =
+            options && options.reflectToAttribute || false;
+        const readOnly: boolean = options && options.readOnly || false;
 
+        let type;
+        if (options && options.hasOwnProperty('type')) {
+            type = options.type;
+        } else if (
+            (window as any).Reflect && Reflect.hasMetadata && Reflect.getMetadata &&
+            Reflect.hasMetadata('design:type', proto, propName)) {
+            type = Reflect.getMetadata('design:type', proto, propName);
+        } else {
+            console.error(
+                'A type could not be found for ${propName}. ' +
+                'Set a type or configure Metadata Reflection API support.');
+        }
 
-export function domElement<T>() {
-  return (proto: any, propName: string) : any => {
-  }
+        if (!proto.constructor.hasOwnProperty('properties')) {
+            proto.constructor.properties = {};
+        }
+        proto.constructor.properties[propName] = {
+            type,
+            notify,
+            reflectToAttribute,
+            readOnly,
+        };
+    }
 }
 
 /**
@@ -68,35 +92,57 @@ export function domElement<T>() {
  * This function must be invoked to return a decorator.
  */
 export function observe(targets: string|string[]) {
-  return (proto: any, propName: string) : any => {
-    const targetString = typeof targets === 'string' ? targets : targets.join(',');
-    const config = _ensureConfig(proto);
-    config.observers.push(`${propName}(${targetString})`);
-  }
+    return (proto: any, propName: string): any => {
+        const targetString =
+            typeof targets === 'string' ? targets : targets.join(',');
+        if (!proto.constructor.hasOwnProperty('observers')) {
+            proto.constructor.observers = [];
+        }
+        proto.constructor.observers.push(`${propName}(${targetString})`);
+    }
 }
 
-interface Config {
-  properties: {[name: string]: PropertyDefinition};
-  observers: string[];
-}
+/**
+ * A TypeScript property decorator factory that converts a class property into
+ * a getter that executes a querySelector on the element's shadow root.
+ *
+ * By annotating the property with the correct type, element's can have
+ * type-checked access to internal elements.
+ *
+ * This function must be invoked to return a decorator.
+ */
+export const query = _query(
+    (target: NodeSelector, selector: string) => target.querySelector(selector));
 
-interface PropertyDefinition {
-  notify?: boolean;
-  type: Function;
-}
+/**
+ * A TypeScript property decorator factory that converts a class property into
+ * a getter that executes a querySelectorAll on the element's shadow root.
+ *
+ * By annotating the property with the correct type, element's can have
+ * type-checked access to internal elements. The type should be NodeList
+ * with the correct type argument.
+ *
+ * This function must be invoked to return a decorator.
+ */
+export const queryAll = _query(
+    (target: NodeSelector, selector: string) =>
+        target.querySelectorAll(selector));
 
-function _ensureConfig(proto: any): Config {
-  const ctor = proto.constructor;
-  if (ctor.hasOwnProperty('__polymer_ts_config')) {
-    return ctor.__polymer_ts_config;
-  }
-
-  Object.defineProperty(ctor, 'config', {
-    get() { return ctor.__polymer_ts_config; }
-  });
-
-  const config: Config = ctor.__polymer_ts_config = ctor.__polymer_ts_config || {};
-  config.properties = config.properties || {};
-  config.observers = config.observers || [];
-  return config;
-}
+/**
+ * Creates a decorator function that accepts a selector, and replaces a
+ * property with a getter than executes the selector with the given queryFn
+ *
+ * @param queryFn A function that executes a query with a selector
+ */
+function _query(
+    queryFn: (target: NodeSelector, selector: string) =>
+        Element | NodeList | null) {
+    return (selector: string) => (proto: any, propName: string): any => {
+        Object.defineProperty(proto, propName, {
+            get(this: HTMLElement) {
+                return queryFn(this.shadowRoot!, selector);
+            },
+            enumerable: true,
+            configurable: true,
+        });
+    };
